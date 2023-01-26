6bb52f9dd89286bbb4ef589f5f3d5df4a7e4eaf0
Jonathan Tri <jonathantric@gmail.com>
2023-01-26 15:15:19 +0700

    Initial Commit

---
+++ Acourse/AcourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcourseApp.swift
+//  Acourse
+//
+//  Created by JonathanTriC on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AcourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
